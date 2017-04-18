a1db48c22e22b93bd8e4a40b06b03a9d6d8c6c37
Anna Madej <aniamadej123@gmail.com>
2017-04-18 20:40:48 +0200

    added click counter

---
+++ spa/03-JavaScriptBasics/js/clicks.js
@@ -0,0 +1,15 @@
+	var clicks = 0;
+
+
+function countClicks()
+{
+        clicks += 1;
+        console.log("You clicked the button " + clicks + " times");
+ 
+	
+}
+
+
+
+
+
---
+++ spa/03-JavaScriptBasics/js/date.js
@@ -0,0 +1,6 @@
+function showDate()
+{
+var date = new Date();
+var d = date.toDateString();
+alert(d);
+}
